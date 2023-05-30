9fc2fc654b6e553be5c799e174ca17e8f7e3f8df
MELISSA MAZIR <melissamazir07@gmail.com>
2023-05-30 15:14:44 +0100

    change javascript object and array by classes and fixing linters error and updating the Readme file

--- script.js
+++ script.js
@@ -1,63 +1,48 @@
-'user strict';
-
-// Retrieve book collection from localStorage or initialize an empty array
-const books = JSON.parse(localStorage.getItem('books')) || [];
-
-// Function to render the book list
-function renderBookList() {
-  const bookListElement = document.getElementById('bookList');
-  bookListElement.innerHTML = '';
-
-  if (books.length === 0) {
-    bookListElement.innerHTML = 'No books in the collection.';
-    return;
+class BookCollection {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem("books")) || [];
   }
 
-  const ul = document.createElement('ul');
-  books.forEach((book, index) => {
-    const li = document.createElement('li');
-    li.innerHTML = `${book.title} <br> ${book.author} <br>`;
+  renderBookList() {
+    const bookListElement = document.getElementById("bookList");
+    bookListElement.innerHTML = "";
 
-    // Function to remove a book from the collection
-    function removeBook(index) {
-      books.splice(index, 1);
-      localStorage.setItem('books', JSON.stringify(books));
-      renderBookList();
+    if (this.books.length === 0) {
+      bookListElement.innerHTML = "No books in the collection.";
+      return;
     }
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.addEventListener('click', () => removeBook(index));
 
-    li.appendChild(removeButton);
-    ul.appendChild(li);
-    ul.appendChild(document.createElement('hr'));
-  });
+    const ul = document.createElement("ul");
+    this.books.forEach((book, index) => {
+      const li = document.createElement("li");
+      li.innerHTML = `"${book.title}" by ${book.author} `;
 
-  bookListElement.appendChild(ul);
-}
+      const removeButton = document.createElement("button");
+      removeButton.innerText = "Remove";
+      removeButton.addEventListener("click", () => this.removeBook(index));
 
-// Function to add a new book to the collection
-function addBook(title, author) {
-  const newBook = { title, author };
-  books.push(newBook);
-  localStorage.setItem('books', JSON.stringify(books));
-  renderBookList();
-}
+      li.appendChild(removeButton);
+      ul.appendChild(li);
+    });
 
-// Handle form submission
-document.getElementById('bookForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  const title = titleInput.value.trim();
-  const author = authorInput.value.trim();
-
-  if (title && author) {
-    addBook(title, author);
-    titleInput.value = '';
-    authorInput.value = '';
+    bookListElement.appendChild(ul);
   }
-});
 
-// Initial rendering of the book list
-renderBookList();
+  addBook(title, author) {
+    const newBook = { title, author };
+    this.books.push(newBook);
+    localStorage.setItem("books", JSON.stringify(this.books));
+    this.renderBookList();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    localStorage.setItem("books", JSON.stringify(this.books));
+    this.renderBookList();
+  }
+}
+
+const bookCollection = new BookCollection();
+// Handle form submission
+
+bookCollection.renderBookList();
