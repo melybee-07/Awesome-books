645d9f7f28e7791b27619e8fc779314c80033f75
SAZZAD1Q2 <kurulia115@gmail.com>
2023-05-31 16:55:54 +0600

    script.js file updated

--- script.js
+++ script.js
@@ -44,5 +44,17 @@ class BookCollection {
 
 const bookCollection = new BookCollection();
 // Handle form submission
+document.getElementById("bookForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const titleInput = document.getElementById("title");
+  const authorInput = document.getElementById("author");
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
 
+  if (title && author) {
+    bookCollection.addBook(title, author);
+    titleInput.value = "";
+    authorInput.value = "";
+  }
+});
 bookCollection.renderBookList();
